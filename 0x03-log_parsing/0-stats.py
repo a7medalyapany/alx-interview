3c115d31b708e0b79e71fd97dc0c46f574fa730d
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2024-05-16 04:25:02 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
-'''0-stats.py'''
-
-
 import sys
 import signal
-import re
 
-
-total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+# Initialize variables
+total_size = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
 def print_stats():
-    '''print_stats'''
-    print("File size:", total_file_size)
-    for status_code in sorted(status_code_counts.keys()):
-        count = status_code_counts[status_code]
-        if count > 0:
-            print(status_code, ":", count)
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
 def signal_handler(sig, frame):
+    """Handles keyboard interruption"""
     print_stats()
     sys.exit(0)
 
+# Handle keyboard interruption
 signal.signal(signal.SIGINT, signal_handler)
 
-for line in sys.stdin:
-    line = line.strip()
-    match = re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$', line)
-    if match:
-        status_code = int(match.group(1))
-        file_size = int(match.group(2))
-        total_file_size += file_size
-        status_code_counts[status_code] += 1
-        line_count += 1
-
-        # Print stats every 10 lines
-        if line_count % 10 == 0:
+if __name__ == "__main__":
+    for i, line in enumerate(sys.stdin, 1):
+        try:
+            data = line.split()
+            size = data[-1]
+            status_code = data[-2]
+            total_size += int(size)
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except Exception:
+            pass
+        if i % 10 == 0:
             print_stats()
+    print_stats()
