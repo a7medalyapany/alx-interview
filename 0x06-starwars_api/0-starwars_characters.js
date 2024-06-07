3962556aed4a57da652cf6868084d9435868f3b3
Ahmed Ibrahim Ahmed Abdullah <103336732+a7medalyapany@users.noreply.github.com>
2024-06-07 03:48:41 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,49 +3,26 @@
 const request = require('request');
 const movieId = process.argv[2];
 
-function getMovieCharacters(movieId) {
-  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-  request.get(filmsUrl, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-      if (movieData) {
-        const charactersUrls = movieData.characters;
-        const characters = [];
-
-        for (const charUrl of charactersUrls) {
-          request.get(charUrl, (error, response, body) => {
-            if (response.statusCode === 200) {
-              const characterData = JSON.parse(body);
-              characters.push(characterData.name);
-              if (characters.length === charactersUrls.length) {
-                printCharacters(characters, movieData.title);
-              }
-            } else {
-              console.log(`Error: ${response.statusCode}`);
-            }
-          });
-        }
-      } else {
-        console.log(`Movie ID ${movieId} not found.`);
-      }
+function printCharacters(characters, i) {
+  request(characters[i], function (error, response, body) {
+    if (error) {
+      console.error('error:', error);
     } else {
-      console.log(`Error: ${response.statusCode}`);
+      console.log(JSON.parse(body).name);
+      if (i + 1 < characters.length) {
+        printCharacters(characters, i + 1);
+      }
     }
   });
-}
-
-function printCharacters(characters, movieTitle) {
-  console.log(`Characters in ${movieTitle}:`);
-  for (const character of characters) {
-    console.log(character);
-  }
-}
-
-if (movieId) {
-  getMovieCharacters(movieId);
-} else {
-  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
 }
