bad015bfcc4ebc3556358e4ab2db9069202b1a91
Ahmed Ibrahim Ahmed Abdullah <103336732+a7medalyapany@users.noreply.github.com>
2024-06-07 03:44:35 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 if (!movieId) {
   console.error('Please provide a Movie ID as the first argument.');
